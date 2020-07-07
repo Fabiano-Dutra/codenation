5e9f307f6087e7463af6192995d29d0d9c6c80e8
Fabiano Dutra <fdutra.oliveira@gmail.com>
2020-07-07 18:31:25 -0300

    Descobri que para fazer o teste funcionar não devo utilizar o botão run do Pycharm e sim rodar no terminal com o comando: python manage.py test Deixei o código de lado pois não estava conseguindo corrigir a mensagem de erro gerada ao acionar o botão run do pycharm. Apendendo mais uma.

--- Modulo_05/products/tests.py
+++ Modulo_05/products/tests.py
@@ -1,16 +1,16 @@
 from django.test import TestCase
-
-from Modulo_05.products.models import Product
+from .models import Product
 
 # Create your tests here.
 class ProductStrTestCase(TestCase):
     def test_str_should_return_name(self):
         # Qualquer módulo tem um gerenciador que é acessado através de objects
-        # utilizando o gerenciado consegue criar, filtrar, deletar e outras operações
+        # utilizando o gerenciado consegue criar, filtrar, deletar e outras operações.
         product = Product.objects.create(
-            nome='Teste Produto',
+            name='Teste Produto',
             description='Teste description',
             price=10.5
         )
+
         self.assertEqual(str(product), 'Teste Produto')
 
