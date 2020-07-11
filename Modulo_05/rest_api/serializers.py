0daa0ed6c0d03597ed9ee4a0e9286e9673d79d1c
Fabiano Dutra <fdutra.oliveira@gmail.com>
2020-07-11 18:34:35 -0300

    Criado API para Categorias e seu serializer.

--- Modulo_05/rest_api/serializers.py
+++ Modulo_05/rest_api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 
-from products.models import Product
+from products.models import Product, Category
 
 class ProductModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields =['id', 'name', 'description', 'price', 'category']
+
+
+# Serializer para a categoria
+class CategoryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
--- Modulo_05/rest_api/urls.py
+++ Modulo_05/rest_api/urls.py
@@ -9,11 +9,13 @@ router = routers.DefaultRouter()
 # Criando a rota da minha api de produtos, usando 'r' rejects, dever ser tratado como uma string regular.
 # Indicar qual url irá usar, no caso é produts, e a view utilizada na sequencia.
 router.register(r'products', views.ProductApiViewSet)
-#router.register(r'categories', views.CategoryApi)
+
 
 
 #Configurando urlpatterns
 urlpatterns = [
     # Na rota abaixo inclui todas as rotas que eu criar usando o router.register nas linhas acima.
     path('',include(router.urls)),
+    # Rota para CategoryApiViews
+    path('categories/', views.CategoryListOnlyAPIView.as_view()),
 ]
